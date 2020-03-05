c1199ef7fb65d47751600dc691becf9a81747e61
waltersantos-muniz <walter.ms.santos41@gmail.com>
2020-03-06 02:18:28 +0900

    Project's Initial Commit

--- www/components/contar.js
+++ www/components/contar.js
@@ -1,38 +1,14 @@
 // This is a JavaScript file
-$(document).on("click","#somar",function(){
-  var valor1 = $("#valor1").val();
-  var valor2 = $("#valor2").val();
-  var result = parseFloat(valor1)+parseFloat(valor2);
-  $("#resultado").val(result);
-
+$(document).on("click","#calc",function(){
+   let resultado = $("#valor3").val()*$("#valor2").val()/$("#valor1").val();
+   $("#resultado").val(resultado);
+    $("#valor4").val(resultado);
 });
-
-$(document).on("click","#subt",function(){
-  var valor1 = $("#valor1").val();
-  var valor2 = $("#valor2").val();
-  var result = parseFloat(valor1)-parseFloat(valor2);
-  $("#resultado").val(result);
-
- }); 
-
-$(document).on("click","#divisao",function(){
-  var valor1 = $("#valor1").val();
-  var valor2 = $("#valor2").val();
-  var result = parseFloat(valor1)/parseFloat(valor2);
-  $("#resultado").val(result);
-
- }); 
-
-$(document).on("click","#multi",function(){
-  var valor1 = $("#valor1").val();
-  var valor2 = $("#valor2").val();
-  var result = parseFloat(valor1)*parseFloat(valor2);
-  $("#resultado").val(result);
-
- }); 
 
 $(document).on("click","#limpar",function(){
   $("#valor1").val("");
    $("#valor2").val("");
-    $("#resultado").val("");
+     $("#valor3").val("");
+       $("#valor4").val("");
+        $("#resultado").val("");
 });
